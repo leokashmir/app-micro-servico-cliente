d445d60878a9f279b8936d6c6968f9cb4f11a3c0
Leo Kashmir <leonardobarrosbhz@gmail.com>
2019-12-04 11:50:28 -0300

    new start page

---
+++ src/main/java/br/com/servico/cliente/controller/StartController.java
@@ -0,0 +1,15 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/")
+public class StartController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+	 public RedirectView conusltaAPI() {
+	    return new RedirectView("https://app-micro-servico-cliente.herokuapp.com/swagger-ui.html");
+	}
+
+}
